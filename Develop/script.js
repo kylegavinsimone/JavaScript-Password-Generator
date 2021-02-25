a3d890a3b104459fff78ae68c4de79481ed76660
kylegavinsimone <kylesimone@outlook.com>
2021-02-25 08:05:38 -0700

    creating notes to self on what is needed so i can keep track of what i have added and still need to

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Bits of code from research online that will be useful below
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// lowercase
+
+// uppercase
+
+// numeric
+
+// special characters
+
