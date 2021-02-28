98184c5bbfa95369a052345ded0130dd2707055b
kylegavinsimone <kylesimone@outlook.com>
2021-02-28 09:05:40 -0700

    Had error on colsole, listen event for clipboard, removed

--- Develop/script.js
+++ Develop/script.js
@@ -71,6 +71,3 @@ function getRandomSymbol() {
 
 const floating_btn = document.querySelector(".floating-btn");
 const close_btn = document.querySelector(".close-btn");
-const social_panel_container = document.querySelector(
-  ".social-panel-container"
-);
